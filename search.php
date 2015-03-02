9494289120b142a7a5f67013b2152283c1d6fabd
Pride <pride.mokhele@gmail.com>
2015-03-02 10:34:17 +0200

    * placed search box in the middle

--- search.php
+++ search.php
@@ -77,7 +77,7 @@ function initialize() {
   // Create the search box and link it to the UI element.
   var input = /** @type {HTMLInputElement} */(
       document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
   var searchBox = new google.maps.places.SearchBox(
     /** @type {HTMLInputElement} */(input));
